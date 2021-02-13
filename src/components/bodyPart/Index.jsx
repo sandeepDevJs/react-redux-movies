26ef71feb05796170044def348f2eba603633080
Sandeep Gupta <sandeepguptabc@gmail.com>
2021-02-13 21:41:25 +0530

    component set up

--- src/components/bodyPart/Index.jsx
+++ src/components/bodyPart/Index.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import Movie from "./Movie";
+import { connect } from "react-redux";
+import { getMoviesAction } from "../../actions/movieActions";
 
-const Index = () => {
-	return (
-		<div className="ui padded grid">
-			<div className="ui stackable four column grid">
-				<Movie />
-				<Movie />
-				<Movie />
-				<Movie src="https://2l7g9kgsh281akevs49v281d-wpengine.netdna-ssl.com/wp-content/uploads/2019/06/Spider-Man-Far-From-Home-Trailer-e1561734444434-1132x670.jpg" />
+class Index extends Component {
+	render() {
+		return (
+			<div className="ui padded grid">
+				<div className="ui stackable four column grid">
+					<Movie />
+					<Movie />
+					<Movie />
+					<Movie src="https://2l7g9kgsh281akevs49v281d-wpengine.netdna-ssl.com/wp-content/uploads/2019/06/Spider-Man-Far-From-Home-Trailer-e1561734444434-1132x670.jpg" />
+				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
 
-export default Index;
+const mapStateToProps = (state) => {
+	return { moviesState: state.moviesState };
+};
